f5e8450ca326c55b38bd44c059fff05a9bd176d3
unknown <you@example.com>
2025-11-10 10:07:10 +0600

    improve: tution fee calculation moved to object method

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from utility import get_tution_fee
 class Student:
     def __init__(self, n):
         self.n = n
-    
+    def tution_fees(self):
+        return get_tution_fee(10)
 
 s = Student("abc")
-print(get_tution_fee(10))
 p = Student("Def")
 q = Student("xyz")
