c62785db5740e60ea2d58489c955d03ce1dcbd26
unknown <you@example.com>
2025-11-10 10:05:17 +0600

    feat: tution fees added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from utility import get_tution_fee
+
 class Student:
     def __init__(self, n):
         self.n = n
+    
 
 s = Student("abc")
+print(get_tution_fee(10))
 p = Student("Def")
 q = Student("xyz")
---
+++ utility.py
@@ -0,0 +1,2 @@
+def get_tution_fee(class_num):
+    return class_num * 3.1416
